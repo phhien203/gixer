f14ac2281c9da1d6e4412cf376cbc4cc1a83a254
Pham Huu Hien <phhien203@gmail.com>
2022-08-06 22:49:40 +0700

    chore: Make appComponent standalone

--- apps/gixer-app/src/app/app.component.ts
+++ apps/gixer-app/src/app/app.component.ts
@@ -2,8 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'gixer-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  template: `
+    <div class="font-mono">
+      <header class="px-xl py-md bg-primary-light text-xl font-bold shadow-md">
+        Gixer
+      </header>
+
+      <main
+        class="max-w-xl md:max-w-2xl lg:max-w-6xl mx-auto py-xl px-md md:px-xl grid grid-cols-1 gap-md md:grid-cols-2 lg:grid-cols-3"
+      >
+        <p>App component works</p>
+      </main>
+    </div>
+  `,
+  styles: [],
 })
 export class AppComponent {
   title = 'gixer-app';
