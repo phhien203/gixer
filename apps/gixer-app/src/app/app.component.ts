e383fdde693855d537b407b43dcaedf3f49bd3c9
Pham Huu Hien <phhien203@gmail.com>
2022-08-07 10:11:27 +0700

    chore: Add taiga-ui package

--- apps/gixer-app/src/app/app.component.ts
+++ apps/gixer-app/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'gixer-root',
   standalone: true,
+  imports: [RouterModule],
   template: `
     <div class="font-mono">
       <header class="px-xl py-md bg-primary-light text-xl font-bold shadow-md">
@@ -12,7 +14,7 @@ import { Component } from '@angular/core';
       <main
         class="max-w-xl md:max-w-2xl lg:max-w-6xl mx-auto py-xl px-md md:px-xl grid grid-cols-1 gap-md md:grid-cols-2 lg:grid-cols-3"
       >
-        <p>App component works</p>
+        <router-outlet></router-outlet>
       </main>
     </div>
   `,
--- libs/users/feat-search-users/src/lib/users-feat-search-users.component.ts
+++ libs/users/feat-search-users/src/lib/users-feat-search-users.component.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiInputModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'gixer-users-search-users',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <p>users-feat-search-users works!</p> `,
+  imports: [CommonModule, TuiInputModule],
+  template: `
+    <tui-input>
+      Enter Github user name
+      <input tuiTextfield type="text" />
+    </tui-input>
+    <p>users-feat-search-users works!</p>
+  `,
   styles: [
     `
       :host {
