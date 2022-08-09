3c7117681a7876277190bfb7bfd5220388ef22c4
Pham Huu Hien <phhien203@gmail.com>
2022-08-09 07:38:18 +0700

    doc: Add JSDoc for abortable operator

--- libs/users/util/src/lib/operators/abortable.operator.ts
+++ libs/users/util/src/lib/operators/abortable.operator.ts
@@ -1,5 +1,15 @@
 import { Observable } from 'rxjs';
 
+/**
+ * The Octokit REST API client returns promises but supports passing an AbortSignal to abort an HTTP request.
+ * Wrapping requests in RxJS observables in this way makes GitHub API requests abortable,
+ * meaning they are canceled when no subscribers remain.
+ *
+ * Ref: The Angular Developer's Nx Handbook, page 61, chapter Creating a data access library
+ * (https://leanpub.com/the-angular-developers-nx-handbook)
+ * @param promiseFactory
+ * @returns Observable
+ */
 export function abortable<TValue>(
   promiseFactory: (signal: AbortSignal) => Promise<TValue>,
 ): Observable<TValue> {
